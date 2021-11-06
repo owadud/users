ed6de020dde120dd3241d63de9aea8b583fff4d9
owadud <akashahmed104466@gmail.com>
2021-11-06 23:14:50 +0600

    done

--- src/Components/Added/Added.js
+++ src/Components/Added/Added.js
@@ -1,18 +1,15 @@
 import React from 'react';
 
 const Added = (props) => {
-    const addUsers =props.addUsers;
+    const addUsers = props.addUsers;
 
-    const salary=addUsers.reduce((sum,user) => sum + user.location.street.number,0)
+    const salary = addUsers.reduce((sum, user) => sum + user.location.street.number, 0)
 
-
-    
-    
     return (
         <div>
             <h1>Added user:{addUsers.length}</h1>
 
-             <h2>Total Added users Salary :${salary}</h2>   
+            <h2>Total Added users Salary :${salary}</h2>
 
         </div>
     );
--- src/Components/User/User.js
+++ src/Components/User/User.js
@@ -1,22 +1,19 @@
 import React from 'react';
 
 const User = (props) => {
-    const addUser =props.addUser;
-    const salary=50000;
-    
-    
-    const{name,email,cell,picture,location}= props.user;
-    const userStyle={
-        border: '2px solid black',width:'20%',padding: '10px',margin:'10px',float: 'left',height:'400px'
+    const addUser = props.addUser;
+    const { name, email, cell, picture, location } = props.user;
+    const userStyle = {
+        border: '2px solid black', width: '20%', padding: '10px', margin: '10px', float: 'left', height: '400px'
     }
     return (
         <div style={userStyle}>
             <img src={picture.large} alt="" />
-            <h3>{name.first +' ' + name.last}</h3> 
+            <h3>{name.first + ' ' + name.last}</h3>
             <h3>Mobile:  {cell}</h3>
             <h4>Email: {email}</h4>
             <h2>Salary:${location.street.number}</h2>
-            <button onClick={()=>addUser(props.user)}>Add user</button>
+            <button onClick={() => addUser(props.user)}>Add user</button>
         </div>
     );
 };
