6e66697a488d8737cb49e6f158c59a174ab50f15
owadud <akashahmed104466@gmail.com>
2021-11-07 19:19:37 +0600

    update

--- src/Components/User/User.js
+++ src/Components/User/User.js
@@ -1,19 +1,22 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
 
 const User = (props) => {
     const addUser = props.addUser;
     const { name, email, cell, picture, location } = props.user;
     const userStyle = {
-        border: '2px solid black', width: '20%', padding: '10px', margin: '10px', float: 'left', height: '400px'
+        border: '2px solid black', width: '20%', padding: '10px', margin: '10px', float: 'left', height: '450px'
     }
     return (
+        
         <div style={userStyle}>
             <img src={picture.large} alt="" />
             <h3>{name.first + ' ' + name.last}</h3>
-            <h3>Mobile:  {cell}</h3>
-            <h4>Email: {email}</h4>
+            <p>Mobile:  {cell}</p>
+            <p>Email: {email}</p>
             <h2>Salary:${location.street.number}</h2>
-            <button onClick={() => addUser(props.user)}>Add user</button>
+            <Button variant="success"  onClick={() => addUser(props.user)}>Add user</Button>
+            
         </div>
     );
 };
